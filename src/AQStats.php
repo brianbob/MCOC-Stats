1dc1143ec249d41d8473b18e96fae4d40afb8377
Brian Bridge <brianjbridge@gmail.com>
2018-01-29 20:09:22 -0500

    Adding the framework for updating the code to be object oriented

---
+++ src/AQStats.php
@@ -0,0 +1,31 @@
+<?php
+
+class AQStats {
+
+	var $bg;
+	// Do we need all these still?
+	var $rows;
+  	var $user_data;
+  	var $points_array;
+  	var $rounds_array;
+  	var $misses_array;
+  	var $users_array;
+  	var $aq_array;
+  	var $top_ten_array;
+  	var $maps_array;
+  	var $explored_array;
+  	var $individual_points_array;
+
+	function __construct() {
+
+	}
+
+	function generateStats() {
+
+	}
+
+	function createChart($chart) {
+		// Return form array
+	}
+
+}
