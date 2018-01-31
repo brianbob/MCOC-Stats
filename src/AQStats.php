f82bb2de0ca9c5a3c7d43c7db557099d82353e45
Brian Bridge <brianjbridge@gmail.com>
2018-01-30 23:21:12 -0500

    Working on the class switchover

--- src/AQStats.php
+++ src/AQStats.php
@@ -1,31 +1,98 @@
 <?php
 
+/*
+ *  IDEAS:
+ *   - Should we pass the form as an object so we can add to it here?
+ */
+
 class AQStats {
 
-	var $bg;
-	// Do we need all these still?
-	var $rows;
-  	var $user_data;
-  	var $points_array;
-  	var $rounds_array;
-  	var $misses_array;
-  	var $users_array;
-  	var $aq_array;
-  	var $top_ten_array;
-  	var $maps_array;
-  	var $explored_array;
-  	var $individual_points_array;
+	private $bg;
+	private $query;
+	private $current_only;
+	private $records;
+	private $user;
+	private $stats_array;
+	// Do we need all these still? These should probably be part of stats_array
+	//private $rows;
+  //private $user_data;
+  //private $points_array;
+  //private $rounds_array;
+  //private $misses_array;
+  //private $users_array;
+  //private $aq_array;
+  //private $top_ten_array;
+  //private $maps_array;
+  //private $explored_array;
+  //private $individual_points_array;
+  //private $top_ten_only;
 
-	function __construct() {
-
+  /**
+   *  Class constructor.
+   *
+   *  boolean $current
+   *    Whether or not to include only current BG membbers.
+   *  int $bg
+   *    The battlegroup number to generate stats for.
+   *  int $user
+   *    This should be the uid if loading an individual user, NULL loads all users
+   */
+	function __construct($current = TRUE, $bg = 2, $user = FALSE) {
+	  $this->current_only = $current;
+	  $this->top_ten_only = $top_ten;
+	  $this->bg = $bg;
+	  $this->stats_array = array();
+    // If type is an integer, then load that user, otherwise NULL to load all users.
+    $this->user = is_int($user) ? $user : NULL;
 	}
 
+
+  /**
+   *  Get the reoords necessary for our stats.
+   *
+   *  I think we should update this so that each chart has it's own generate function, and each
+   *  one calls this function to get the records it needs. To do this I think we should use a lot
+   *  of caching so each chart that needs individual records doesn't repeat the query.
+   */
+	function queryRecords()) {
+	  $query->entityCondition('entity_type', 'node')
+	    ->entityCondition('bundle', $type);
+
+    // @TODO move this to the charts function. Maybe it should be it's own function?
+	  //if($this->$top_ten) {
+	  //  $query->fieldOrderBy('field_points', 'value', 'DESC')->range(0, $range);
+	  //}
+
+	  if (!is_null($this->$user)) {
+	    $query->fieldCondition('field_member', 'target_id', $user);
+	  }
+
+	  if (!is_null($this->$bg)) {
+	    $query->fieldCondition('field_bg_', 'value', $bg);
+	  }
+
+	  $records = $query->execute();
+	  $this->records = $records['node'];
+	}
+
+
+  /**
+   *  Iterate over $this->records and fill out $this->stats_array.
+   */
 	function generateStats() {
-
+    foreach ($this->records as $record) {
+      // @TODO calculate stuff here.
+    }
 	}
 
+
+  /**
+   *  Create a chart. Return a form array for insertion into #markup.
+   */
 	function createChart($chart) {
-		// Return form array
+    // $this->queryRecords($arguments_here);
+    // $this->generateStats($arguments_here);
+    // Return form array here.
 	}
 
 }
